7a6dfc8fd1e4af558fffc7fba4e583580c6a4779
Jisu-Woniu <31986081+Jisu-Woniu@users.noreply.github.com>
2023-11-09 15:55:47 +0800

    refactor: :recycle: Extract gen_key_pair from write_key_pair.

--- src-tauri/crypto/src/keygen.rs
+++ src-tauri/crypto/src/keygen.rs
@@ -5,7 +5,10 @@ use std::{
 };
 
 use ed25519::{
-    pkcs8::{spki::der::pem::LineEnding, EncodePrivateKey, EncodePublicKey},
+    pkcs8::{
+        spki::der::{pem::LineEnding, zeroize::Zeroizing},
+        EncodePrivateKey, EncodePublicKey,
+    },
     KeypairBytes, PublicKeyBytes,
 };
 use ed25519_dalek::SigningKey;
@@ -31,16 +34,7 @@ pub async fn write_key_pair(path: impl AsRef<Path>) -> Result<()> {
         let priv_key = path.with_file_name(priv_name);
         let pub_key = path.with_file_name(pub_name);
 
-        let key_pair =
-            KeypairBytes::from_bytes(&SigningKey::generate(&mut thread_rng()).to_keypair_bytes());
-
-        let priv_key_pem = key_pair
-            .to_pkcs8_der()?
-            .to_pem("PRIVATE KEY", LineEnding::default())?;
-
-        let pub_key_pem = PublicKeyBytes::try_from(&key_pair)?
-            .to_public_key_der()?
-            .to_pem("PUBLIC KEY", LineEnding::default())?;
+        let (priv_key_pem, pub_key_pem) = gen_keypair()?;
 
         try_join!(
             write_secret_file(&priv_key, priv_key_pem.as_bytes()),
@@ -52,40 +46,29 @@ pub async fn write_key_pair(path: impl AsRef<Path>) -> Result<()> {
     Ok(())
 }
 
+fn gen_keypair() -> Result<(Zeroizing<String>, String)> {
+    let key_pair =
+        KeypairBytes::from_bytes(&SigningKey::generate(&mut thread_rng()).to_keypair_bytes());
+    let priv_key_pem = key_pair
+        .to_pkcs8_der()?
+        .to_pem("PRIVATE KEY", LineEnding::default())?;
+    let pub_key_pem = PublicKeyBytes::try_from(&key_pair)?
+        .to_public_key_der()?
+        .to_pem("PUBLIC KEY", LineEnding::default())?;
+    Ok((priv_key_pem, pub_key_pem))
+}
+
 #[cfg(test)]
 mod tests {
-    use std::{env, io, path::PathBuf};
 
-    use rand::{distributions::Alphanumeric, Rng};
-
-    use tokio::fs;
-
-    use crate::error::Result;
-
-    use super::write_key_pair;
-
-    pub async fn tmp_dir(prefix: impl AsRef<str>) -> io::Result<PathBuf> {
-        let mut inner = env::temp_dir();
-        let s: String = {
-            // shrink scope of rng
-            let rng = rand::thread_rng();
-            rng.sample_iter(Alphanumeric)
-                .map(char::from)
-                .take(10)
-                .collect()
-        };
-
-        inner.push(&format!("{}-{}", prefix.as_ref(), s));
-
-        fs::create_dir(&inner).await?;
-        Ok(inner)
-    }
+    use super::{gen_keypair, Result};
 
     #[tokio::test]
     #[ignore = "Manual testing for file generation."]
     async fn test() -> Result<()> {
-        let tmp = tmp_dir("keygen").await?;
-        write_key_pair(dbg!(tmp).join("key")).await?;
+        let (priv_key, pub_key) = gen_keypair()?;
+        println!("{}", *priv_key);
+        println!("{}", pub_key);
         Ok(())
     }
 }
