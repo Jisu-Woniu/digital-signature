c1417cd04b110d07f2da6c56e12170d6f726f7b5
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2026-03-15 22:07:36 +0800

    build(dep): migrate to oxc in vite config, remove unused deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { URL, fileURLToPath } from "node:url";
 
-import autoPrefixer from "autoprefixer";
 import { defineConfig } from "vite";
 import icons from "unplugin-icons/vite";
 import postcssPresetEnv from "postcss-preset-env";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig(async () => ({
-  plugins: [vue(), icons({ compiler: "vue3" })],
+export default defineConfig({
+  plugins: [
+    vue(),
+    icons({
+      compiler: "vue3",
+    }),
+  ],
 
   clearScreen: false,
 
@@ -20,7 +24,7 @@ export default defineConfig(async () => ({
   envPrefix: ["VITE_", "TAURI_ENV_*"],
   css: {
     postcss: {
-      plugins: [postcssPresetEnv, autoPrefixer],
+      plugins: [postcssPresetEnv],
     },
   },
   resolve: {
@@ -29,11 +33,8 @@ export default defineConfig(async () => ({
     },
   },
   build: {
-    // Tauri uses Edge on Windows and WebKit on macOS and Linux
-    target:
-      process.env.TAURI_ENV_PLATFORM === "windows" ? "edge105" : "safari13",
     // don't minify for debug builds
-    minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
+    minify: !process.env.TAURI_ENV_DEBUG ? "oxc" : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
@@ -41,4 +42,4 @@ export default defineConfig(async () => ({
     // Remove Vue Options API support for memory consumption.
     __VUE_OPTIONS_API__: false,
   },
-}));
+});
