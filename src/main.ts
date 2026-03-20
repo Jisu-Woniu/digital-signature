7a5cc5850bc2875bb0d111427a379013effb6cff
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2026-03-20 16:53:30 +0800

    feat: 💄 use MD3 blueprint

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import "vuetify/styles";
+import "./styles.css";
 import App from "@/App.vue";
 import "@fontsource/noto-sans-sc/chinese-simplified-300.css";
 import "@fontsource/noto-sans-sc/chinese-simplified-400.css";
@@ -7,12 +9,11 @@ import "@fontsource/roboto/latin-400.css";
 import "@fontsource/roboto/latin-400-italic.css";
 import { createApp } from "vue";
 import { createVuetify } from "vuetify";
+import { md3 } from "vuetify/blueprints";
 import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
-import "vuetify/styles";
-
-import "./styles.css";
 
 const vuetify = createVuetify({
+  blueprint: md3,
   icons: { aliases, defaultSet: "mdi", sets: { mdi } },
 });
 
